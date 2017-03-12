cb651b79d82d9f45fba1b0fca7365cf5ed7e7c0c
Ty Coghlan <coghlan.ty@gmail.com>
2017-03-12 14:33:58 -0400

    set up logging, seems easy to iterate through entire subreddit

--- build.sbt
+++ build.sbt
@@ -7,3 +7,5 @@ scalaVersion := "2.12.1"
 resolvers += Resolver.jcenterRepo
 
 libraryDependencies += "net.dean.jraw" % "JRAW" % "0.9.0"
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.1.7"
+libraryDependencies += "com.typesafe.scala-logging" %% "scala-logging" % "3.5.0"
--- src/main/scala-2.12/cog/ty/scraper/DailyProgrammerScraper.scala
+++ src/main/scala-2.12/cog/ty/scraper/DailyProgrammerScraper.scala
@@ -1,7 +1,16 @@
 package cog.ty.scraper
 
+import com.typesafe.scalalogging
+import java.io.File
+
+import com.typesafe.scalalogging.Logger
 import net.dean.jraw.RedditClient
 import net.dean.jraw.http.UserAgent
+import net.dean.jraw.http.oauth.{Credentials, OAuthHelper}
+import net.dean.jraw.models.{Listing, Submission}
+import net.dean.jraw.paginators.{Sorting, SubredditPaginator, TimePeriod}
+
+import scala.io.Source
 
 /**
   * Created by ty on 3/12/17.
@@ -11,6 +20,28 @@ class DailyProgrammerScraper {
 }
 
 object Demo extends App {
+  val logger = Logger("Demo")
   val userAgent = UserAgent.of("desktop", "cog.ty", "v0.1", "NiceGuy_Ty")
+  val classLoader = getClass.getClassLoader
+  val credentialsFile = new File(classLoader.getResource("credentials.conf").getFile)
+  val credentials = Source.fromFile(credentialsFile).getLines().next().split(" ")
+  val scriptCredentials = Credentials.script(credentials(0), credentials(1), credentials(2), credentials(3))
   val redditClient = new RedditClient(userAgent)
+  val authData = redditClient.getOAuthHelper.easyAuth(scriptCredentials)
+  redditClient.authenticate(authData)
+  logger.info(redditClient.me().hasVerifiedEmail.toString)
+  logger.info(redditClient.getCurrentRatelimit.toString)
+
+  val dailyProgrammer = new SubredditPaginator(redditClient, "dailyprogrammer")
+  dailyProgrammer.setTimePeriod(TimePeriod.ALL)
+  dailyProgrammer.setSorting(Sorting.NEW)
+  dailyProgrammer.setLimit(100)
+
+  import collection.JavaConverters._
+  while (dailyProgrammer.hasNext) {
+    val submissions: Listing[Submission] = dailyProgrammer.next()
+    for ((s: Submission) <- submissions.iterator().asScala) {
+      logger.info(s"\n  Author: ${s.getAuthor}\n  Score: ${s.getScore}\n  Created: ${s.getCreated}\n  ${s.getTitle}")
+    }
+  }
 }
