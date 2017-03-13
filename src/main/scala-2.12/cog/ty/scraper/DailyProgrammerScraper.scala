dc86a643b7e84ab37c092bd7098e7d3016e4ec98
Ty Coghlan <coghlan.ty@gmail.com>
2017-03-12 22:29:56 -0400

    some work done on initial scraping

--- src/main/scala-2.12/cog/ty/scraper/DailyProgrammerScraper.scala
+++ src/main/scala-2.12/cog/ty/scraper/DailyProgrammerScraper.scala
@@ -2,6 +2,8 @@ package cog.ty.scraper
 
 import com.typesafe.scalalogging
 import java.io.File
+import java.net.URL
+import java.util.Date
 
 import com.typesafe.scalalogging.Logger
 import net.dean.jraw.RedditClient
@@ -10,6 +12,7 @@ import net.dean.jraw.http.oauth.{Credentials, OAuthHelper}
 import net.dean.jraw.models.{Listing, Submission}
 import net.dean.jraw.paginators.{Sorting, SubredditPaginator, TimePeriod}
 
+import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 
 /**
@@ -39,9 +42,42 @@ object Demo extends App {
 
   import collection.JavaConverters._
   while (dailyProgrammer.hasNext) {
+    val users = ArrayBuffer[User]()
+    val challenges = ArrayBuffer[Challenge]()
+    val challengeResponses = ArrayBuffer[ChallengeResponse]()
     val submissions: Listing[Submission] = dailyProgrammer.next()
     for ((s: Submission) <- submissions.iterator().asScala) {
-      logger.info(s"\n  Author: ${s.getAuthor}\n  Score: ${s.getScore}\n  Created: ${s.getCreated}\n  ${s.getTitle}")
+      val validChallenge = """\[(\d\d\d\d)(-|/)(\d\d)(-|/)(\d\d)\] Challenge #(\d)+ \[w+\]""".r
+      s.getTitle match {
+        case validChallenge(_*) =>
+          logger.debug(s"Successfully matched regex for `${s.getTitle}`")
+          parseSubmission(s, users, challenges, challengeResponses)
+        case _ =>
+          logger.debug(s"Could not parse submission with title `${s.getTitle}`")
+      }
     }
   }
+
+  def parseSubmission(s: Submission, users: ArrayBuffer[User], challenges: ArrayBuffer[Challenge], challengeResponses: ArrayBuffer[ChallengeResponse]) = {
+    val challenge = Challenge(s.getId, s.getCreated, permaLinkToUrl(s.getPermalink), s.getTitle, s.getSelftext, Other("Not implemented at the moment"))
+    logger.debug(s"Challenge Parsed: $challenge")
+
+    val commentNode = s.getComments
+    for (c <- commentNode.walkTree()) {
+
+    }
+  }
+
+  def permaLinkToUrl(relativePath: String): URL = new URL("reddit.com/" + relativePath)
 }
+
+case class User(id: String, username: String)
+case class Challenge(id: String, created: Date, link: URL, title: String, body: String, difficulty: Difficulty)
+case class Response(id: String, created: Date, link: URL, body: String, karma: Int)
+case class ChallengeResponse(id: String, challenge: Challenge, user: User, responses: Seq[Response])
+
+sealed trait Difficulty
+case object Easy extends Difficulty
+case object Intermediate extends Difficulty
+case object Hard extends Difficulty
+case class Other(tag: String) extends Difficulty
