8ef7497f98eb5219f18bd07aae2123468f78af4d
Ty <Ty_Coghlan@hms.harvard.dedu>
2017-03-14 16:10:55 -0400

    finished first draft of scraper

--- src/main/scala-2.12/cog/ty/scraper/DailyProgrammerScraper.scala
+++ src/main/scala-2.12/cog/ty/scraper/DailyProgrammerScraper.scala
@@ -1,7 +1,7 @@
 package cog.ty.scraper
 
 import com.typesafe.scalalogging
-import java.io.File
+import java.io.{BufferedWriter, File}
 import java.net.URL
 import java.util.Date
 
@@ -9,26 +9,36 @@ import com.typesafe.scalalogging.Logger
 import net.dean.jraw.RedditClient
 import net.dean.jraw.http.UserAgent
 import net.dean.jraw.http.oauth.{Credentials, OAuthHelper}
-import net.dean.jraw.models.{Listing, Submission}
+import net.dean.jraw.models.{
+  Account,
+  Comment,
+  CommentNode,
+  Listing,
+  Submission
+}
 import net.dean.jraw.paginators.{Sorting, SubredditPaginator, TimePeriod}
 
+import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 
 /**
   * Created by ty on 3/12/17.
   */
-class DailyProgrammerScraper {
-
-}
+class DailyProgrammerScraper {}
 
 object Demo extends App {
   val logger = Logger("Demo")
   val userAgent = UserAgent.of("desktop", "cog.ty", "v0.1", "NiceGuy_Ty")
   val classLoader = getClass.getClassLoader
-  val credentialsFile = new File(classLoader.getResource("credentials.conf").getFile)
-  val credentials = Source.fromFile(credentialsFile).getLines().next().split(" ")
-  val scriptCredentials = Credentials.script(credentials(0), credentials(1), credentials(2), credentials(3))
+  val credentialsFile = new File(
+    classLoader.getResource("credentials.conf").getFile)
+  val credentials =
+    Source.fromFile(credentialsFile).getLines().next().split(" ")
+  val scriptCredentials = Credentials.script(credentials(0),
+                                             credentials(1),
+                                             credentials(2),
+                                             credentials(3))
   val redditClient = new RedditClient(userAgent)
   val authData = redditClient.getOAuthHelper.easyAuth(scriptCredentials)
   redditClient.authenticate(authData)
@@ -41,43 +51,116 @@ object Demo extends App {
   dailyProgrammer.setLimit(100)
 
   import collection.JavaConverters._
+
+  val users = ArrayBuffer[User]()
+  val challenges = ArrayBuffer[Challenge]()
+  val responses = ArrayBuffer[Response]()
+
   while (dailyProgrammer.hasNext) {
-    val users = ArrayBuffer[User]()
-    val challenges = ArrayBuffer[Challenge]()
-    val challengeResponses = ArrayBuffer[ChallengeResponse]()
     val submissions: Listing[Submission] = dailyProgrammer.next()
+
     for ((s: Submission) <- submissions.iterator().asScala) {
-      val validChallenge = """\[(\d\d\d\d)(-|/)(\d\d)(-|/)(\d\d)\] Challenge #(\d)+ \[w+\]""".r
+      val validChallenge =
+        """\[\d+(-|/)\d+(-|/)\d+] Challenge #\d+ \[(\w+)].*""".r
       s.getTitle match {
-        case validChallenge(_*) =>
+        case validChallenge(_, _, difficulty) =>
           logger.debug(s"Successfully matched regex for `${s.getTitle}`")
-          parseSubmission(s, users, challenges, challengeResponses)
+          parseSubmission(s, Difficulty(difficulty)).foreach {
+            case (challenge, user, response) =>
+              challenges += challenge
+              users += user
+              responses += response
+              logger.debug("Successfully parsed a response with challenge id"
+                + s"`${challenge.id}`, user id `${user.id}`, and response id `${response.id}`")
+          }
         case _ =>
-          logger.debug(s"Could not parse submission with title `${s.getTitle}`")
+          logger.debug(
+            s"Could not parse submission with title `${s.getTitle}`")
       }
     }
   }
 
-  def parseSubmission(s: Submission, users: ArrayBuffer[User], challenges: ArrayBuffer[Challenge], challengeResponses: ArrayBuffer[ChallengeResponse]) = {
-    val challenge = Challenge(s.getId, s.getCreated, permaLinkToUrl(s.getPermalink), s.getTitle, s.getSelftext, Other("Not implemented at the moment"))
-    logger.debug(s"Challenge Parsed: $challenge")
+  val bw = new BufferedWriter(
+    new java.io.FileWriter(
+      new File(classLoader.getResource("out.txt").getFile),
+      false))
 
-    val commentNode = s.getComments
-    for (c <- commentNode.walkTree()) {
+  bw.write("")
+  bw.append("CHALLENGES")
+  bw.append("-------------")
+  bw.append(challenges.map(ch => ch.id + ": " + ch.title).mkString("\n"))
+  bw.append("USERS")
+  bw.append("-------------")
+  bw.append(users.map(us => us.id + ": " + us.username).mkString("\n"))
+  bw.append("RESPONSES")
+  bw.append("-------------")
+  bw.append(
+    responses
+      .map(r =>
+        s"${r.challenge.id} -> ${r.user.id} -> ${r.id}:"
+          + s"${r.body.substring(0, math.min(r.body.length, 10))}")
+      .mkString("\n"))
+  bw.flush()
+  bw.close()
 
+  def parseSubmission(
+      s: Submission,
+      difficulty: Difficulty): Iterable[(Challenge, User, Response)] = {
+    val challenge = Challenge(s.getId,
+                              s.getCreated,
+                              permaLinkToUrl(s.getPermalink),
+                              s.getTitle,
+                              s.getSelftext,
+                              difficulty)
+    for ((c: CommentNode) <- s.getComments.walkTree().asScala) yield {
+      val comment = c.getComment
+      val user =
+        User(redditClient.getUser(comment.getAuthor).getId, comment.getAuthor)
+      val response = Response(comment.getId,
+                              challenge,
+                              user,
+                              comment.getCreated,
+                              new URL(comment.getUrl),
+                              comment.getBody)
+      (challenge, user, response)
     }
   }
 
-  def permaLinkToUrl(relativePath: String): URL = new URL("reddit.com/" + relativePath)
+  def permaLinkToUrl(relativePath: String): URL =
+    new URL("reddit.com/" + relativePath)
 }
 
 case class User(id: String, username: String)
-case class Challenge(id: String, created: Date, link: URL, title: String, body: String, difficulty: Difficulty)
-case class Response(id: String, created: Date, link: URL, body: String, karma: Int)
-case class ChallengeResponse(id: String, challenge: Challenge, user: User, responses: Seq[Response])
+
+case class Challenge(id: String,
+                     created: Date,
+                     link: URL,
+                     title: String,
+                     body: String,
+                     difficulty: Difficulty)
+
+case class Response(id: String,
+                    challenge: Challenge,
+                    user: User,
+                    created: Date,
+                    link: URL,
+                    body: String)
 
 sealed trait Difficulty
+
 case object Easy extends Difficulty
+
 case object Intermediate extends Difficulty
+
 case object Hard extends Difficulty
+
 case class Other(tag: String) extends Difficulty
+
+object Difficulty {
+  def apply(str: String): Difficulty = str.toLowerCase match {
+    case "easy" => Easy
+    case "intermediate" => Intermediate
+    case "hard" => Hard
+    case _ => Other(str.toLowerCase)
+  }
+}
